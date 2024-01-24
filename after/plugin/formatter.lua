bc10182f7f495265a6939728b3370c6c9efacb00
Heesang Whang <hswhang@anvil.kr>
2024-01-24 09:08:13 +0900

    Feat: Setup prettier autoformat

---
+++ after/plugin/formatter.lua
@@ -0,0 +1,12 @@
+local autocmd_group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+
+-- Format JavaScript and TypeScript files after saving (use Prettier)
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json" },
+    desc = "Auto-format JavaScript and TypeScript files after saving",
+    callback = function()
+        local fileName = vim.api.nvim_buf_get_name(0)
+        vim.cmd(":!prettier --log-level silent --write " .. fileName)
+    end,
+    group = autocmd_group
+})
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,7 @@ lsp.preset("recommended")
 lsp.ensure_installed({
     'tsserver',
     'rust_analyzer',
+    'prettier',
 })
 
 -- Fix Undefined global 'vim'
@@ -80,8 +81,6 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-    lsp.buffer_autoformat()
 end)
 
 lsp.setup()
