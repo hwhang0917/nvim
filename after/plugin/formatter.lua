8becb07acda9f0d1c3a7401d2034544c3f5a1986
Heesang Whang <hswhang@anvil.kr>
2024-01-24 13:00:12 +0900

    Update: Add common & module js to formatter

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -2,7 +2,7 @@ local autocmd_group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = t
 
 -- Format JavaScript and TypeScript files after saving (use Prettier)
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json" },
+    pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.mjs", "*.cjs" },
     desc = "Auto-format JavaScript and TypeScript files after saving",
     callback = function()
         local fileName = vim.api.nvim_buf_get_name(0)
