9449dd3be5bc66e15d6236e6897370246c0ee8f3
Heesang Whang <hwhang0917@gmail.com>
2024-02-11 13:20:21 +0900

    Fix: Change git blame toggle

--- after/plugin/blame.lua
+++ after/plugin/blame.lua
@@ -5,4 +5,7 @@ require("gitblame").setup {
 -- Date Format
 vim.g.gitblame_date_format = '%r'
 -- Blame format
-vim.g.gitblame_message_template = '  <author> • <date> • [<sha>] <summary>'
+vim.g.gitblame_message_template = '  <author> • <date> • <summary>'
+
+-- Toggle Git Blame
+vim.keymap.set("n", "<leader>gb", function() vim.cmd("GitBlameToggle") end, { silent = true })
