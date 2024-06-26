cea636ea277291371121d7c18caa0c4c03875d57
Heesang Whang <hwhang0917@gmail.com>
2024-06-26 12:51:45 +0900

    Update: change lualine theme

--- lua/runfridge/plugins/lualine.lua
+++ lua/runfridge/plugins/lualine.lua
@@ -1,17 +1,4 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup {
-            options = { theme = 'tokyonight' },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
-            },
-        }
-    end
 }
