ef9998f72d9b843e55322fa22da39a7d58792df7
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-06-09 23:25:01 +0900

    Fix: Revert back to prettier

--- lua/runfridge/plugins/lsp/conform.lua
+++ lua/runfridge/plugins/lsp/conform.lua
@@ -1,43 +1,20 @@
 return {
 	"stevearc/conform.nvim",
 	config = function()
-		local os = GetOS()
-		local formatters_config = {}
-		if os == "windows" then
-			formatters_config.biome = {
-				command = vim.fn.stdpath("data") .. "/mason/bin/biome.exe",
-				timeout_ms = 5000,
-			}
-            -- setup orphaned biome taskkill
-            vim.api.nvim_create_autocmd("VimLeavePre", {
-                callback = function()
-                    vim.fn.system("taskkill /F /IM biome.exe 2>nul")
-                end
-            })
-		elseif os == "macos" or os == "linux" then
-			formatters_config.biome = {
-				command = vim.fn.stdpath("data") .. "/mason/bin/biome",
-			}
-		else
-			formatters_config.biome = {
-				command = "biome",
-			}
-		end
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-				json = { "biome" },
-				graphql = { "biome" },
-				markdown = { "biome" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				json = { "jq" },
+				graphql = { "prettier" },
+				markdown = { "prettier" },
 				html = { "prettier" },
-				css = { "biome" },
-				vue = { "biome" },
+				css = { "prettier" },
+				vue = { "prettier" },
 			},
-			formatters = formatters_config,
 			-- format_on_save = {
 			-- 	timeout_ms = 10000,
 			-- 	lsp_fallback = true,
