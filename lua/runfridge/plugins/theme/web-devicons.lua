1ee020f640d790da9036ee30937e4f1d7b98ba70
RunFridge <33582774+hwhang0917@users.noreply.github.com>
2025-06-19 16:59:33 +0900

    Fix: broken css devicon

--- lua/runfridge/plugins/theme/web-devicons.lua
+++ lua/runfridge/plugins/theme/web-devicons.lua
@@ -3,12 +3,25 @@ return {
 	lazy = true,
 	config = function()
 		require("nvim-web-devicons").setup({
+            default = true,
 			override = {
+				js = {
+					icon = "",
+                    color = "#f1e05a",
+                    cterm_color = "185",
+					name = "Js",
+				},
 				ts = {
-					icon = "󰛦",
+					icon = "",
 					color = "#519aba",
 					cterm_color = "74",
 					name = "Ts",
+				},
+				css = {
+					icon = "",
+					color = "#3d8fc6",
+					cterm_color = "74",
+					name = "Css",
 				},
 			},
 		})
