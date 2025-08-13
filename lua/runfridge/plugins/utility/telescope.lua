bb57e23952da722fe83d911c5105edb0a4f5fc54
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-08-13 12:07:57 +0900

    Fix: change telescope-fzf-native build from cmake to make

--- lua/runfridge/plugins/utility/telescope.lua
+++ lua/runfridge/plugins/utility/telescope.lua
@@ -1,20 +1,20 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "0.1.5",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release",
-		},
-	},
-	cmd = "Telescope",
-	config = function()
-		require("telescope").setup({
-			defaults = {
-				disable_devicons = false,
-				path_display = { "truncate" },
-			},
-		})
-	end,
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.5",
+    dependencies = {
+        { "nvim-lua/plenary.nvim" },
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make"
+        },
+    },
+    cmd = "Telescope",
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                disable_devicons = false,
+                path_display = { "truncate" },
+            },
+        })
+    end,
 }
