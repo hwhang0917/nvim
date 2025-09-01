15a9b9298d2d41b96ecff83f72bac049b26892a2
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-09-01 09:12:25 +0900

    Fix: Garbage day config change

--- lua/runfridge/plugins/misc/garbage.lua
+++ lua/runfridge/plugins/misc/garbage.lua
@@ -3,9 +3,28 @@ return {
 	dependencies = "neovim/nvim-lspconfig",
 	event = "VeryLazy",
 	opts = {
-		-- Milliseconds to wait before restoring LSP after the mouse re-enters nvim
+		-- Main options
+		aggressive_mode = false,
+		grace_period = 60 * 15, -- 15 minutes (default)
 		wakeup_delay = 0,
-		-- Seconds to wait before stopping all LSP clients after neovim loses focus
-		grace_period = 60 * 30,
+
+		-- Debugging options
+		notifications = false, -- Set to true for debugging
+		retries = 3,
+		timeout = 1000,
+
+		-- LSP clients that should never be stopped
+		excluded_lsp_clients = {
+			"null-ls",
+			"jdtls",
+			"marksman",
+			"lua_ls",
+		},
+
+		-- Buffers to ignore in aggressive mode
+		aggressive_mode_ignore = {
+			filetype = { "", "markdown", "text", "org", "tex", "asciidoc", "rst" },
+			buftype = { "nofile" },
+		},
 	},
 }
