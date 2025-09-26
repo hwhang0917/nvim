dedb102e872d89aadb90a037767f450b3babbc27
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-09-26 09:31:09 +0900

    chore: remove garbage-day

--- lua/runfridge/lazy.lua
+++ lua/runfridge/lazy.lua
@@ -28,33 +28,32 @@ Add("runfridge.plugins.lsp.lsp")
 Add("runfridge.plugins.lsp.treesitter")
 -- Misc
 Add("runfridge.plugins.misc.wakatime")
-Add("runfridge.plugins.misc.garbage")
 Add("runfridge.plugins.misc.plenary")
 Add("runfridge.plugins.misc.bloat")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        lazyrepo,
-        lazypath
-    })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(PLUGIN_SPECS, {
-    checker = { enabled = true, notify = false },
+	checker = { enabled = true, notify = false },
 })
