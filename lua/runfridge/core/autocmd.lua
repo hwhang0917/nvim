0e6bcedd400cc60de4ed7abccaa9997ec7d1e862
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2025-10-31 13:37:16 +0900

    update: remove trim whitespace autocmd

--- lua/runfridge/core/autocmd.lua
+++ lua/runfridge/core/autocmd.lua
@@ -1,11 +1,11 @@
 local api = vim.api
 
 --- Remove all trailing whitespace on save
-local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
-api.nvim_create_autocmd("BufWritePre", {
-	command = [[:%s/\s\+$//e]],
-	group = TrimWhiteSpaceGrp,
-})
+-- local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+-- api.nvim_create_autocmd("BufWritePre", {
+-- 	command = [[:%s/\s\+$//e]],
+-- 	group = TrimWhiteSpaceGrp,
+-- })
 
 -- Highlight on yank
 api.nvim_create_autocmd("TextYankPost", {
