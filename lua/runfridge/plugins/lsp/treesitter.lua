d125b5fac91e4852a87b4b1c0715b4b4926a273d
hwhang0917 <33582774+hwhang0917@users.noreply.github.com>
2026-02-24 17:43:52 +0900

    fix: treesitter update api change

--- lua/runfridge/plugins/lsp/treesitter.lua
+++ lua/runfridge/plugins/lsp/treesitter.lua
@@ -1,37 +1,34 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installted = {
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"c",
-				"lua",
-				"rust",
-				"yaml",
-				"json",
-				"html",
-				"css",
-				"bash",
-				"python",
-				"go",
-				"tsx",
-				"markdown",
-				"vue"
-			},
-			sync_install = false,
-			auto_install = false,
-			ignore_install = {},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = {
+                "vimdoc",
+                "javascript",
+                "typescript",
+                "c",
+                "lua",
+                "rust",
+                "yaml",
+                "json",
+                "html",
+                "css",
+                "bash",
+                "python",
+                "go",
+                "tsx",
+                "markdown",
+                "vue",
+            },
+        })
+
+        -- highlighting and indent are now built-in to Neovim
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+    end,
 }
